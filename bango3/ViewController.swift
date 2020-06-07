3559d1cffb6cbc9fd42c6bb5ab5303b34c759e0d
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-06-07 11:41:57 +0900

    Initial Commit

---
+++ bango3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bango3
+//
+//  Created by Chihiro Nishiwaki on 2020/06/07.
+//  Copyright © 2020 Nishiwaki sen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bango3Tests/bango3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  bango3Tests.swift
+//  bango3Tests
+//
+//  Created by Chihiro Nishiwaki on 2020/06/07.
+//  Copyright © 2020 Nishiwaki sen. All rights reserved.
+//
+
+import XCTest
+@testable import bango3
+
+class bango3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
